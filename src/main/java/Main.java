51112b544d3598cec4d439feb3a0862e959e379c
Andrea Morra <andreamorra01@gmail.com>
2021-05-03 15:39:19 +0200

    Commenti sui test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,10 @@ public class Main {
     public static void main(String[] args) {
         StringaEstesa str = new StringaEstesa("");
         System.out.println(str.isBlank());
+        System.out.println(str.isNUmeric());
+        System.out.println(str.isEmpty());
     }
+
 
 }
 
--- src/test/java/StringaEstesaTest.java
+++ src/test/java/StringaEstesaTest.java
@@ -7,19 +7,29 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 class StringaEstesaTest {
 
-
+    /**
+     * fa il test sul metodo isEmpty quando la  stringa è vuota
+     */
     @Test
     void isEmptyNull() {
         StringaEstesa str = new StringaEstesa();
         Assertions.assertTrue(str.isEmpty());
         Assertions.assertEquals(str.isEmpty(), StringUtils.isEmpty(str.getValue()));
     }
+
+    /**
+     * fa il test fa il test sul metodo isEmpty quando la  stringa ha un valore
+     */
     @Test
     void isEmptyWithValue() {
         StringaEstesa str = new StringaEstesa("nsnsn");
         Assertions.assertFalse(str.isEmpty());
         Assertions.assertEquals(str.isEmpty(), StringUtils.isEmpty(str.getValue()));
     }
+
+    /**
+     * fa il test sul metodo isEmpty quando la  stringa ha un valore numerico
+     */
     @Test
     void isEmptyNumbers() {
         StringaEstesa str = new StringaEstesa("1");
@@ -27,18 +37,29 @@ class StringaEstesaTest {
         Assertions.assertEquals(str.isEmpty(), StringUtils.isEmpty(str.getValue()));
     }
 
+    /**
+     * fa il test sul metoodo isBlank quando la stringa è vuota
+     */
     @Test
     void isBlankStringEmpty() {
         StringaEstesa str = new StringaEstesa("");
         Assertions.assertTrue(str.isBlank());
         Assertions.assertEquals( str.isBlank(), StringUtils.isBlank(str.getValue()) );
     }
+
+    /**
+     * fa il test sul metoodo isBlank quando la stringa ha almeno uno spazio
+     */
     @Test
     void isBlankOnlySpaces() {
         StringaEstesa str = new StringaEstesa(" ");
         Assertions.assertTrue(str.isBlank());
         Assertions.assertEquals( str.isBlank(), StringUtils.isBlank(str.getValue()) );
     }
+
+    /**
+     * fa il test sul metoodo isBlank quando la stringa ha un valore
+     */
     @Test
     void isBlankWithValue() {
         StringaEstesa str = new StringaEstesa(" 2s");
@@ -46,12 +67,19 @@ class StringaEstesaTest {
         Assertions.assertEquals( str.isBlank(), StringUtils.isBlank(str.getValue()) );
     }
 
+    /**
+     * fa il test sul metodo isNumeric quando la stringa contiene solo numeri
+     */
     @Test
     void isNUmericOnlyNumbers() {
             StringaEstesa str = new StringaEstesa("939393");
             Assertions.assertTrue(str.isNUmeric());
             Assertions.assertEquals(str.isNUmeric(), StringUtils.isNumeric(str.getValue()) );
     }
+
+    /**
+     * fa il test sul metodo isNumeric quando la stringa contiene numeri e lettere
+     */
     @Test
     void isNUmericWithLetters() {
         StringaEstesa str = new StringaEstesa("hshs");
@@ -59,9 +87,22 @@ class StringaEstesaTest {
         Assertions.assertEquals(str.isNUmeric(), StringUtils.isNumeric(str.getValue()) );
     }
 
+    /**
+     * fa il test sul metodo isNumeric quando la stringa contiene numeri spazi
+     */
     @Test
     void isNUmericWithSpaces() {
         StringaEstesa str = new StringaEstesa("1 2 ");
+        Assertions.assertFalse(str.isNUmeric());
+        Assertions.assertEquals(str.isNUmeric(), StringUtils.isNumeric(str.getValue()) );
+    }
+
+    /**
+     * fa il test sul metodo isNumeric quando la stringa è vuota
+     */
+    @Test
+    void isNumericNull() {
+        StringaEstesa str = new StringaEstesa("");
         Assertions.assertFalse(str.isNUmeric());
         Assertions.assertEquals(str.isNUmeric(), StringUtils.isNumeric(str.getValue()) );
     }
